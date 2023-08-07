60c8efd84a60b6b238d327ea45c4a4a687e4a2ec
ArthurBuback <103044946+ArthurBuback@users.noreply.github.com>
2023-08-07 08:48:47 -0300

    Add files via upload

--- Seguradora/src/modelo/Cliente.java
+++ Seguradora/src/modelo/Cliente.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author alunodev08
- */
 public class Cliente {
+    /**
+     * String é maiúsculo pois faz menção ao objeto String.
+     * String é um objeto primitivo, ou seja é algo que existe no padrão do Java(?).
+     * E como muitas pessoas utilizam muito, fizeram a própria "classe" String.
+     */
+    private String nome;
+    private String cpf;
+
+    // Não é necessário escrever essa proxima parte, pode-se apenas clicar como botão direito e em "Insert Code".
+    // Este é o "Constructor"
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    //
+    
+    // Esses são os "Getters and Setters"
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    //
     
 }
--- Seguradora/src/modelo/Seguro.java
+++ Seguradora/src/modelo/Seguro.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author alunodev08
- */
 public class Seguro {
+    private float valor;
+    private boolean vigencia;
+    // Esta é uma das relações com Cliente:
+    private Cliente proprietario;
+    // O nome desta classe abaixo deve ser o nome do arquivo (não é obrigatório porém é uma boa prática) que ela faz referencia, sendo uma maneira alternativade de referenciar, baseado na anterior.
+    private Cliente[] assegurados;
+
+    public Seguro(float valor, boolean vigencia, Cliente proprietario) {
+        this.valor = valor;
+        this.vigencia = vigencia;
+        this.proprietario = proprietario;
+    }
+
+    public Cliente[] getAssegurados() {
+        return assegurados;
+    }
+
+    public void setAssegurados(Cliente[] assegurados) {
+        this.assegurados = assegurados;
+    }
+    
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public boolean isVigencia() {
+        return vigencia;
+    }
+
+    public void setVigencia(boolean vigencia) {
+        this.vigencia = vigencia;
+    }
+
+    public Cliente getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(Cliente proprietario) {
+        this.proprietario = proprietario;
+    }
+    
     
 }
